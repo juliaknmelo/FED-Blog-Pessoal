9edd2863bbed1102c70cc506d6e2e0787e1a2746
juliaknmelo <ju-kar98@hotmail.com>
2024-06-25 23:01:49 -0300

    testando hooks e props

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
-import React from 'react';
-import Home from './paginas/home/Home';
+import React, { useState } from 'react';
+// import Home from './paginas/home/Home';
 import './App.css';
 
 
 function App() {
+
+const [valor, setValor] = useState(0);
+
+function handleClick(){
+  setValor(valor + 1);
+}
+
   return (
-   <Home />
+  //  <Home title="Home"
+  //  description="Esta é a melhor cantora pop da atualidade"/>
+
+   <div>
+    <p>O valor é: {valor}</p>
+    <button onClick={handleClick}>Adicionar 1</button>
+   </div>
   );
 }
 
--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import './Home.css';
 
-function Home(){
+
+interface minhaProps {
+    title: string;
+    description: string;
+  }
+
+function Home(props:minhaProps){
     return (
         <>
-            <h1 className='titulo'>Home</h1>
+            <h2 className='titulo'>{props.title}</h2>
             <img src="https://i.imgur.com/VqfHCU6.jpeg" alt="Imagem Tela Inicial" className="img"/>
-           
+           <p>{props.description}</p>
         </>
     );
 }
