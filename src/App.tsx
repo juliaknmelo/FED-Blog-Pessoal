9f77ce19775c6a3441f4df21809dfe8389c220a4
juliaknmelo <ju-kar98@hotmail.com>
2024-06-27 16:09:39 -0300

    att

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,31 @@
 import React from 'react';
 import './App.css';
-import Home from './paginas/home/Home';
+
+import Navbar from './components/navBar/Navbar';
+import Footer from './components/footer/Footer';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Login from './paginas/login/login';
+import Login from './pages/login/login';
+import Cadastro from './pages/cadastro/Cadastro';
+import Home from './pages/home/Home';
+
+
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/home" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
+   
+        <BrowserRouter>
+          <Navbar />
+          <div className='min-h-[80vh]'>
+            <Routes>
+              <Route path="/" element={<Cadastro />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/cadastro" element={<Cadastro />} />
+              <Route path="/home" element={<Home />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+  
     </>
   );
 }
